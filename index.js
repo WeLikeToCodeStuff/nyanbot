1e5f5ec58f7ad9feb28dbd4ccd90e93b32edde74
Ssmidge <adrian.trifonov@gmail.com>
2020-10-17 16:27:40 -0400

    V1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const discord = require("discord.js")
+const bot = new discord.Client();
 
+bot.login("TOKEN")
+
+
+bot.on("ready", () => {
+    console.log(`${bot.user.username} is ready and online!`)
+})
+
+bot.on("message", (message) => {
+    if (!message.content.startsWith("-")) return;
+    if (message.content.startsWith("-nyancat")) {
+        message.channel.send("https://tenor.com/view/nyan-nyancats-nyan-anon-anonymous-anonymous-bites-back-gif-14781718")
+    }
+})
